6a4f8481e5b0f39bf18acabdc477056a4c1e4945
sadekunle <your@email.com>
2026-05-15 08:35:02 +0100

    fix SPL send - use @solana/spl-token official library

--- App.tsx
+++ App.tsx
@@ -48,23 +48,19 @@ async function _sendSOL(pubkey:string,secretKey:Uint8Array,recipient:string,lamp
 }
 
 async function _sendSPL(pubkey:string,secretKey:Uint8Array,recipient:string,amountRaw:number,mint:string):Promise<string>{
-  const {PublicKey,Transaction,SystemProgram,TransactionInstruction} = require('@solana/web3.js');
-  const TOKEN_PROG = new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
-  const ASSOC_PROG = new PublicKey('ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJe1bS4');
-  const SYSVAR_RENT = new PublicKey('SysvarRent111111111111111111111111111111111');
+  const {PublicKey,Transaction,Keypair} = require('@solana/web3.js');
+  const {getAssociatedTokenAddress,createAssociatedTokenAccountInstruction,createTransferInstruction,TOKEN_PROGRAM_ID,ASSOCIATED_TOKEN_PROGRAM_ID} = require('@solana/spl-token');
   const mintPk = new PublicKey(mint);
   const fromPk = new PublicKey(pubkey);
-  const toPk  = new PublicKey(recipient);
-  const [fromATA] = await PublicKey.findProgramAddress([fromPk.toBuffer(),TOKEN_PROG.toBuffer(),mintPk.toBuffer()],ASSOC_PROG);
-  const [toATA]   = await PublicKey.findProgramAddress([toPk.toBuffer(),TOKEN_PROG.toBuffer(),mintPk.toBuffer()],ASSOC_PROG);
+  const toPk = new PublicKey(recipient);
+  const fromATA = await getAssociatedTokenAddress(mintPk, fromPk);
+  const toATA = await getAssociatedTokenAddress(mintPk, toPk);
   const tx = new Transaction();
   const ataInfo = await rpcFetch('getAccountInfo',[toATA.toBase58(),{encoding:'base64'}]);
   if(!ataInfo?.result?.value){
-    tx.add(new TransactionInstruction({keys:[{pubkey:fromPk,isSigner:true,isWritable:true},{pubkey:toATA,isSigner:false,isWritable:true},{pubkey:toPk,isSigner:false,isWritable:false},{pubkey:mintPk,isSigner:false,isWritable:false},{pubkey:SystemProgram.programId,isSigner:false,isWritable:false},{pubkey:TOKEN_PROG,isSigner:false,isWritable:false},{pubkey:SYSVAR_RENT,isSigner:false,isWritable:false}],programId:ASSOC_PROG,data:Buffer.alloc(0)}));
-    
+    tx.add(createAssociatedTokenAccountInstruction(fromPk,toATA,toPk,mintPk,TOKEN_PROGRAM_ID,ASSOCIATED_TOKEN_PROGRAM_ID));
   }
-  const ixData=Buffer.alloc(9);ixData[0]=3;ixData.writeBigUInt64LE(BigInt(amountRaw),1);
-  tx.add(new TransactionInstruction({keys:[{pubkey:fromATA,isSigner:false,isWritable:true},{pubkey:toATA,isSigner:false,isWritable:true},{pubkey:fromPk,isSigner:true,isWritable:false}],programId:TOKEN_PROG,data:ixData}));
+  tx.add(createTransferInstruction(fromATA,toATA,fromPk,amountRaw,TOKEN_PROGRAM_ID));
   const bh = await rpcFetch('getLatestBlockhash',[{commitment:'confirmed'}]);
   tx.recentBlockhash=bh.result.value.blockhash;tx.feePayer=fromPk;
   const sig = nacl.sign.detached(tx.serializeMessage(), secretKey);
